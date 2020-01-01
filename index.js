15ee0ce48c979bbdbbd44443d41983e5576d8e2c
Sean <sjgallen@gmail.com>
2019-12-31 22:27:44 -0600

    updated index/html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let app = require('express')();
 let http = require('http').createServer(app);
 
-app.get('/', (req, res) => res.send('<h1>Hello world</h1>'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
